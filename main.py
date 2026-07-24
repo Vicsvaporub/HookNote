60830d4ae4ef1fc74da541f48e99bd7db885325d
Vicsvaporub <paulawong09@gmail.com>
2026-07-24 12:59:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
+import gi
 
+gi.require_version("Gtk", "4.0")
+
+from gi.repository import Gtk
+
+
+class HookNote(Gtk.Application):
+
+    def __init__(self):
+        super().__init__(
+            application_id="com.vic.hooknote"
+        )
+
+    def do_activate(self):
+
+        window = Gtk.ApplicationWindow(
+            application=self
+        )
+
+        window.set_title(
+            "🧶 HookNote"
+        )
+
+        window.set_default_size(
+            900,
+            600
+        )
+
+        label = Gtk.Label(
+            label="Welcome to HookNote 🧶"
+        )
+
+        window.set_child(
+            label
+        )
+
+        window.present()
+
+
+app = HookNote()
+
+app.run()
